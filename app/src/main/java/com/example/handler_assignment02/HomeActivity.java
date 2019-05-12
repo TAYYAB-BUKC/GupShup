14df17160cbac37470352d9659542e64f1c1f48f
MUHAMMAD WAQAS FAYYAZ <samiwaqas567@outlook.com>
2019-05-12 23:54:44 +0500

    GupShup LogIn Screen -UI

--- app/src/main/java/com/example/handler_assignment02/HomeActivity.java
+++ app/src/main/java/com/example/handler_assignment02/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.handler_assignment02;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
+
 
 public class HomeActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
     }
+
 }
