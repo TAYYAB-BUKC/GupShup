9fde742235895f1f496d5b92d74f2d87ff8021f7
MUHAMMAD WAQAS FAYYAZ <samiwaqas567@outlook.com>
2019-05-13 00:13:21 +0500

    GupShup LogIn Screen -Backend with "Handler"

--- app/src/main/java/com/example/handler_assignment02/HomeActivity.java
+++ app/src/main/java/com/example/handler_assignment02/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.example.handler_assignment02;
 
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 
@@ -15,6 +18,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(HomeActivity.this, "Authenticated Successfully", Toast.LENGTH_LONG).show();
+                    }
+                },4000);
+            }
+        });
 
     }
 
