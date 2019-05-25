249d1baad61aac7fb91a629fb92d7bb89dcd8102
MUHAMMAD WAQAS FAYYAZ <samiwaqas567@outlook.com>
2019-05-25 16:31:22 +0500

    Gupshu - Group Chat -UserInterface In which the user can send the message

--- app/src/main/java/com/example/gupshup/SettingsActivity.java
+++ app/src/main/java/com/example/gupshup/SettingsActivity.java
@@ -1,13 +1,66 @@
 package com.example.gupshup;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @Override
+    private Button UpdateAccountSettings;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage;
+
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+
+    private static final int GalleryPick = 1;
+    private StorageReference UserProfileImagesRef;
+    private ProgressDialog loadingBar;
+
+    private Toolbar SettingsToolBar;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        InitializeFields();
     }
-}
+    private void InitializeFields() {
+        UpdateAccountSettings = (Button) findViewById(R.id.update_settings_button);
+        userName = (EditText) findViewById(R.id.set_user_name);
+        userStatus = (EditText) findViewById(R.id.set_profile_status);
+        userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+    }
+    }
