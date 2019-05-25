80e2cdb795b2d369de6b3150176e32e220f1a593
MUHAMMAD WAQAS FAYYAZ <samiwaqas567@outlook.com>
2019-05-25 14:17:29 +0500

    LogIn User Complete Backend and Frontend Developed

--- app/src/main/java/com/example/gupshup/MainActivity.java
+++ app/src/main/java/com/example/gupshup/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mAuth = FirebaseAuth.getInstance();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
@@ -51,9 +50,6 @@ public class MainActivity extends AppCompatActivity {
             {
                 SendUserToRegisterActivity();
             }
-
-            private void SendUserToRegisterActivity() {
-            }
         });
 
 
@@ -61,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-              //  AllowUserToLogin();
+                AllowUserToLogin();
             }
         });
 
@@ -69,14 +65,111 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Intent phoneLoginIntent = new Intent(MainActivity.this, Register.class);
-                startActivity(phoneLoginIntent);
+               // Intent phoneLoginIntent = new Intent(MainActivity.this, PhoneLoginActivity.class);
+               // startActivity(phoneLoginIntent);
             }
         });
     }
 
-    private void InitializeFields() {
+
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            SendUserToMainActivity();
+        }
     }
 
 
+
+    private void AllowUserToLogin()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait....");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                String currentUserId = mAuth.getCurrentUser().getUid();
+                                String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                UsersRef.child(currentUserId).child("device_token")
+                                        .setValue(deviceToken)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task)
+                                            {
+                                                if (task.isSuccessful())
+                                                {
+                                                    SendUserToMainActivity();
+                                                    Toast.makeText(MainActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                }
+                                            }
+                                        });
+                            }
+                            else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(MainActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+
+    private void InitializeFields()
+    {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
+        loadingBar = new ProgressDialog(this);
+    }
+
+
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(MainActivity.this, Register.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void SendUserToRegisterActivity()
+    {
+        Intent registerIntent = new Intent(MainActivity.this, Register.class);
+        startActivity(registerIntent);
+    }
 }
