fe29de72e4c90a5d547e7097045f3b70ca88aaf9
Xaqlain <39124598+Xaqlain@users.noreply.github.com>
2019-05-26 16:38:52 +0500

    Groups Fragment Activity

--- app/src/main/java/com/example/gupshup/GroupsFragment.java
+++ app/src/main/java/com/example/gupshup/GroupsFragment.java
@@ -38,26 +38,73 @@ public class GroupsFragment extends Fragment {
     private ListView list_view;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_groups = new ArrayList<>();
-    public GroupsFragment() {
+
+    private DatabaseReference GroupRef;
         // Required empty public constructor
-    }
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        groupFragmentView = inflater.inflate(R.layout.fragment_groups, container, false);
+
+
+        GroupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
 
 
         IntializeFields();
+        RetrieveAndDisplayGroups();
+
+
+
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                String currentGroupName = adapterView.getItemAtPosition(position).toString();
+
+                Intent groupChatIntent = new Intent(getContext(), GroupChatActivity.class);
+                groupChatIntent.putExtra("groupName" , currentGroupName);
+                startActivity(groupChatIntent);
+            }
+        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_groups, container, false);
     }
 
-    private void IntializeFields() {
+    private void IntializeFields()
+    {
         list_view = (ListView) groupFragmentView.findViewById(R.id.list_view);
         arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_groups);
         list_view.setAdapter(arrayAdapter);
     }
 
 
+    private void RetrieveAndDisplayGroups()
+    {
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                Set<String> set = new HashSet<>();
+                Iterator iterator = dataSnapshot.getChildren().iterator();
+
+                while (iterator.hasNext())
+                {
+                    set.add(((DataSnapshot)iterator.next()).getKey());
+                }
+
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
+
