aa45667fad18da40b614f442b95e720a8693b2c2
MISBAH HUSSAIN <misbahhussain679@gmail.com>
2019-05-26 16:44:21 +0500

    Import OnCreate method Find-friends activity

--- app/src/main/java/com/example/gupshup/FindFriendsActivity.java
+++ app/src/main/java/com/example/gupshup/FindFriendsActivity.java
@@ -12,7 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -23,9 +24,27 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
     }
-}
+    }
+
